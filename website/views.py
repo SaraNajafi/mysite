b32f128ebd72b93b3a3d839234a3760060130ea6
Sara Najafi <sara.njf.ch@gmail.com>
2022-11-30 00:18:50 +0330

    created frist app
    created first app named website for 3 views

---
+++ website/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse,JsonResponse
+def http_test(request):
+    return HttpResponse('<h1>This is a test </h1>')
+def json_test(request):
+    return JsonResponse({'name': 'ali'})
+def home_view(request):
+    return HttpResponse('<h1>Home Page</h1>')
+def about_view(request):
+    return HttpResponse('<h1>About Page</h1>')
+def contact_view(request):
+    return HttpResponse('<h1>Contact Page</h1>')
