a422688f8a703ee0b76df6dcc11902e4c0759d07
Sara Najafi <sara.njf.ch@gmail.com>
2023-05-29 10:53:16 +0330

    form
    working with django forms

--- website/urls.py
+++ website/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('',home_view, name='home'),
     path('about',about_view, name='about'),
     path('contact',contact_view, name='contact'),
-    path('elements',elements_view, name='elements')
+    path('elements',elements_view, name='elements'),
+    path('test', test_view, name='test'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post
+from website.models import Contact
+from website.forms import NameForm
 # Create your views here.
 from django.http import HttpResponse,JsonResponse
 def http_test(request):
@@ -14,3 +16,13 @@ def contact_view(request):
     return render(request,'website/contact.html')
 def elements_view(request):
     return render(request, 'website/elements.html')
+
+def test_view(request):
+    if request.method == 'POST':
+        form=NameForm(request.POST)
+        if form.is_valid():
+            return HttpResponse('done')
+        
+        
+    form=NameForm()
+    return render(request, 'test.html',{'form': form})
