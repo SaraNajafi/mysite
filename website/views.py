e7eb1bc6449620562f8055db17cc1f3d71bde30d
Sara Najafi <sara.njf.ch@gmail.com>
2023-06-02 18:55:42 +0330

    some changes
    some little changes

--- website/views.py
+++ website/views.py
@@ -4,48 +4,57 @@ from website.models import Contact
 from website.forms import NameForm, ContactForm, NewsLetterForm
 from django.contrib import messages
 # Create your views here.
-from django.http import HttpResponse,JsonResponse, HttpResponseRedirect
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+
+
 def http_test(request):
     return HttpResponse('<h1>This is a test </h1>')
+
+
 def json_test(request):
     return JsonResponse({'name': 'ali'})
+
+
 def home_view(request):
-    return render(request,'website/index.html')
+    return render(request, 'website/index.html')
+
+
 def about_view(request):
-    return render(request,'website/about.html')
+    return render(request, 'website/about.html')
+
+
 def contact_view(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            Contact.objects.create(
-                name='anonymous',
-                email=form.cleaned_data.get('email'),
-                subject =form.cleaned_data.get('subject'),
-                message=form.cleaned_data.get('message')
-            )
-            #form.save()
-            messages.add_message(request, messages.SUCCESS, 'Your ticket submited successfully')
-            #messages.success(request, "Message sent." )
+            newmessage = form.save(commit=False)
+            newmessage.name = 'Uknown'
+            newmessage.save()
+            form.save_m2m()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Your ticket submited successfully')
+            # messages.success(request, "Message sent." )
             return redirect("/")
         else:
             messages.add_message(request, messages.ERROR, 'Invalid')
     form = ContactForm
-    return render(request,'website/contact.html',{'form':form})
+    return render(request, 'website/contact.html', {'form': form})
 
 
 def elements_view(request):
     return render(request, 'website/elements.html')
 
+
 def test_view(request):
     if request.method == 'POST':
-        form=ContactForm(request.POST)
+        form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponse('done')
-        
-        
-    form=ContactForm()
-    return render(request, 'test.html',{'form': form})
+
+    form = ContactForm()
+    return render(request, 'test.html', {'form': form})
+
 
 def newsletter_view(request):
     if request.method == 'POST':
@@ -55,6 +64,3 @@ def newsletter_view(request):
             return HttpResponseRedirect('/')
     else:
         return HttpResponseRedirect('/')
-
-    
-
