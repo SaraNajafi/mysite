293ee28088919913f53f957f1a354f357b572605
Sara Najafi <sara.njf.ch@gmail.com>
2023-06-05 17:11:22 +0330

    step by step ready for production
    production

---
+++ mysite/setting/dev.py
@@ -0,0 +1,42 @@
+from mysite.settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-^4t8qu@p2s^nzf2+7_-$$&vzncz14f$y65@#&&w9hdhkrom)if'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+#INSTALLED_APPS=[]
+
+# sites framework
+SITE_ID = 2
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+
+STATIC_ROOT = BASE_DIR/'static/'
+MEDIA_ROOT = BASE_DIR/'media/'
+
+STATICFILES_DIRS=[
+    BASE_DIR/"statics",
+    
+]
+
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
--- mysite/settings.py
+++ mysite/settings.py
@@ -16,18 +16,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^4t8qu@p2s^nzf2+7_-$$&vzncz14f$y65@#&&w9hdhkrom)if'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
+CSRF_COOKIE_SECURE=True
 # Application definition
 
 INSTALLED_APPS = [
@@ -50,8 +41,7 @@ INSTALLED_APPS = [
     'accounts'
 ]
 
-# sites framework
-SITE_ID = 2
+
 
 #robots
 ROBOTS_USE_HOST=True
@@ -93,15 +83,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -141,15 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/'static/'
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media/'
 
-STATICFILES_DIRS=[
-    BASE_DIR/"statics",
-    
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -160,4 +134,3 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
